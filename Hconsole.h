bd6772bb649c442ef271e0148d318adc1cda4f71
Copy05 <71971770+Copy05@users.noreply.github.com>
2022-03-26 11:44:31 +0100

    echo and more decription

--- Hconsole.h
+++ Hconsole.h
@@ -3,9 +3,13 @@
 #include <Windows.h>
 #include <string>
 
+/*
+	The Hconsole Class which contains everything what Hconsole requires
+	Variables, Functions, Enums, Everything!
+	TODO: Split
+*/
 class Hconsole
 {
-
 // Private field for all commands functions
 private:
 	void whoami()
@@ -23,6 +27,12 @@ private:
 		if (cmd == "whoami") {
 			whoami();
 		}
+		if (cmd == "echo") {
+			std::string ehc;
+			std::cin >> ehc;
+			std::cout << ehc << std::endl;
+			CMD();
+		}
 		if (cmd == "setusername") {
 			std::string newusername;
 			std::cout << "New username: ";
@@ -35,9 +45,10 @@ private:
 			exit(0);
 		}
 		if (cmd == "help") {
-			std::cout << "HELP		- Shows a list with all avaible commands" << std::endl;
-			std::cout << "WHOAMI	- Shows Who you are                     " << std::endl;
-			std::cout << "EXIT		- Ends Hconsole                         " << std::endl;
+			std::cout << "HELP			- Shows a list with all avaible commands" << std::endl;
+			std::cout << "WHOAMI		- Shows Who you are                     " << std::endl;
+			std::cout << "SETUSERNAME	- Changes your Hconsole username        " << std::endl;
+			std::cout << "EXIT			- Ends Hconsole                         " << std::endl;
 			CMD();
 		}
 		else {
@@ -64,6 +75,7 @@ public:
 // Private field for all Hconsole variables
 private:
 
+	// Used for admin commands
 	typedef enum H_adminStatus : short {
 		NO = 0,
 		LIMMITED = 1,
